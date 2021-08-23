6a5bec622153928649c886e4280311bc46e2de20
Zor Max <zormax052@hotmail.com>
2021-08-24 00:09:41 +0300

    Factory Method Design Pattern

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    public interface ICreditCard
+    {
+        string GetCardType();
+
+        int GetCreditLimit();
+
+        int GetAnnualCharge();
+    }
+
+    public class Platinum : ICreditCard
+    {
+        public string GetCardType()
+        {
+            return "Platinum Plus";
+        }
+        public int GetCreditLimit()
+        {
+            return 35000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 2000;
+        }
+    }
+    public class Titanium : ICreditCard
+    {
+        public string GetCardType()
+        {
+            return "Titanium Edge";
+        }
+        public int GetCreditLimit()
+        {
+            return 25000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 1500;
+        }
+    }
+    public class MoneyBack : ICreditCard
+    {
+        public string GetCardType()
+        {
+            return "MoneyBack";
+        }
+        public int GetCreditLimit()
+        {
+            return 15000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 500;
+        }
+    }
+
+    public abstract class CreditCardFactory
+    {
+        protected abstract ICreditCard MakeProduct();
+        public ICreditCard CreateProduct()
+        {
+            return this.MakeProduct();
+        }
+    }
+
+    public class MoneyBackFactory : CreditCardFactory
+    {
+        protected override ICreditCard MakeProduct()
+        {
+            ICreditCard product = new MoneyBack();
+            return product;
+        }
+    }
+    public class PlatinumFactory : CreditCardFactory
+    {
+        protected override ICreditCard MakeProduct()
+        {
+            ICreditCard product = new Platinum();
+            return product;
+        }
+    }
+    public class TitaniumFactory : CreditCardFactory
+    {
+        protected override ICreditCard MakeProduct()
+        {
+            ICreditCard product = new Titanium();
+            return product;
+        }
+    }
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ICreditCard creditCard = new PlatinumFactory().CreateProduct();
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.GetCardType());
+                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            Console.WriteLine("--------------");
+            creditCard = new MoneyBackFactory().CreateProduct();
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.GetCardType());
+                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            Console.ReadLine();
+        }
+    }
+    }
+}
+    
