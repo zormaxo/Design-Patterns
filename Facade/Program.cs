1235379c8f153e1f238bae4e8e0e9d81881cd8a2
Zor Max <zormax052@hotmail.com>
2021-09-10 07:15:33 +0300

    Facade

---
+++ Facade/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+            order.PlaceOrder();
+            Console.Read();
+        }
+    }
+
+    public class Order
+    {
+        public void PlaceOrder()
+        {
+            Console.WriteLine("Place Order Started");
+            Product product = new Product();
+            product.GetProductDetails();
+            Payment payment = new Payment();
+            payment.MakePayment();
+            Invoice invoice = new Invoice();
+            invoice.Sendinvoice();
+            Console.WriteLine("Order Placed Successfully");
+        }
+
+    }
+    public class Product
+    {
+        public void GetProductDetails()
+        {
+            Console.WriteLine("Fetching the Product Details");
+        }
+    }
+
+    public class Payment
+    {
+        public void MakePayment()
+        {
+            Console.WriteLine("Payment Done Successfully");
+        }
+    }
+
+    public class Invoice
+    {
+        public void Sendinvoice()
+        {
+            Console.WriteLine("Invoice Send Successfully");
+        }
+    }
+}
