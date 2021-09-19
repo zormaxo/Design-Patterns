7ffc40274844004206c7efdfb66abcb263739ab2
Zor Max <zormax052@hotmail.com>
2021-09-19 23:48:44 +0300

    Bridge

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -23,76 +23,77 @@ namespace Adapter
                 target.ProcessCompanySalary(employeesArray);
                 Console.Read();
             }
-    }
+        }
 
-    public class EmployeeAdapter : ITarget
-    {
-        ThirdPartyBillingSystem thirdPartyBillingSystem = new ThirdPartyBillingSystem();
-
-        public void ProcessCompanySalary(string[,] employeesArray)
+        public class EmployeeAdapter : ITarget
         {
-            string Id = null;
-            string Name = null;
-            string Designation = null;
-            string Salary = null;
-            List<Employee> listEmployee = new List<Employee>();
-            for (int i = 0; i < employeesArray.GetLength(0); i++)
+            ThirdPartyBillingSystem thirdPartyBillingSystem = new ThirdPartyBillingSystem();
+
+            public void ProcessCompanySalary(string[,] employeesArray)
             {
-                for (int j = 0; j < employeesArray.GetLength(1); j++)
+                string Id = null;
+                string Name = null;
+                string Designation = null;
+                string Salary = null;
+                List<Employee> listEmployee = new List<Employee>();
+                for (int i = 0; i < employeesArray.GetLength(0); i++)
                 {
-                    if (j == 0)
+                    for (int j = 0; j < employeesArray.GetLength(1); j++)
                     {
-                        Id = employeesArray[i, j];
+                        if (j == 0)
+                        {
+                            Id = employeesArray[i, j];
+                        }
+                        else if (j == 1)
+                        {
+                            Name = employeesArray[i, j];
+                        }
+                        else if (j == 2)
+                        {
+                            Designation = employeesArray[i, j];
+                        }
+                        else
+                        {
+                            Salary = employeesArray[i, j];
+                        }
                     }
-                    else if (j == 1)
-                    {
-                        Name = employeesArray[i, j];
-                    }
-                    else if (j == 2)
-                    {
-                        Designation = employeesArray[i, j];
-                    }
-                    else
-                    {
-                        Salary = employeesArray[i, j];
-                    }
+                    listEmployee.Add(new Employee(Convert.ToInt32(Id), Name, Designation, Convert.ToDecimal(Salary)));
                 }
-                listEmployee.Add(new Employee(Convert.ToInt32(Id), Name, Designation, Convert.ToDecimal(Salary)));
+                Console.WriteLine("Adapter converted Array of Employee to List of Employee");
+                Console.WriteLine("Then delegate to the ThirdPartyBillingSystem for processing the employee salary\n");
+                thirdPartyBillingSystem.ProcessSalary(listEmployee);
             }
-            Console.WriteLine("Adapter converted Array of Employee to List of Employee");
-            Console.WriteLine("Then delegate to the ThirdPartyBillingSystem for processing the employee salary\n");
-            thirdPartyBillingSystem.ProcessSalary(listEmployee);
         }
-    }
-    public interface ITarget
-    {
-        void ProcessCompanySalary(string[,] employeesArray);
-    }
-
-    public class ThirdPartyBillingSystem
-    {
-        //ThirdPartyBillingSystem accepts employees information as a List to process each employee salary
-        public void ProcessSalary(List<Employee> listEmployee)
+        public interface ITarget
         {
-            foreach (Employee employee in listEmployee)
+            void ProcessCompanySalary(string[,] employeesArray);
+        }
+
+        public class ThirdPartyBillingSystem
+        {
+            //ThirdPartyBillingSystem accepts employees information as a List to process each employee salary
+            public void ProcessSalary(List<Employee> listEmployee)
             {
-                Console.WriteLine("Rs." + employee.Salary + " Salary Credited to " + employee.Name + " Account");
+                foreach (Employee employee in listEmployee)
+                {
+                    Console.WriteLine("Rs." + employee.Salary + " Salary Credited to " + employee.Name + " Account");
+                }
             }
         }
-    }
 
-    public class Employee
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Designation { get; set; }
-        public decimal Salary { get; set; }
-        public Employee(int id, string name, string designation, decimal salary)
+        public class Employee
         {
-            ID = id;
-            Name = name;
-            Designation = designation;
-            Salary = salary;
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string Designation { get; set; }
+            public decimal Salary { get; set; }
+            public Employee(int id, string name, string designation, decimal salary)
+            {
+                ID = id;
+                Name = name;
+                Designation = designation;
+                Salary = salary;
+            }
         }
     }
 }
