441ce9621f56d6d292b84b2d7ca8c0bbc7593962
Zor Max <okocadere@gmail.com>
2021-11-04 00:07:13 +0300

    Stopwatch Test

---
+++ Common/StopwatchTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TimeTester
+{
+    static class StopwatchTest
+    {
+        static void CheckTime()
+        {
+            Console.WriteLine("Hello World!");
+
+            var list = new List<string>();
+            for (int i = 0; i < Math.Pow(10, 6); i++)
+            {
+                if (i == 1 || i % 2 != 0)
+                {
+                    list.Add(i.ToString());
+                }
+                else
+                {
+                    list.Add(null);
+                }
+            }
+
+            var watch = new Stopwatch();
+            watch.Start();
+
+            foreach (var s in list)
+            {
+                try
+                {
+                    int i = s.Length;
+                }
+                catch (Exception ex)
+                {
+                    string e = ex.Message;
+                    var a = ex.StackTrace;
+                }
+            }
+
+            watch.Stop();
+            Console.WriteLine(watch.Elapsed);
+
+            watch.Restart();
+
+            foreach (var s in list)
+            {
+                try
+                {
+                    int i = s.Length;
+                }
+                catch (Exception ex)
+                {
+                    string e = ex.Message;
+                    string a = ex.StackTrace;
+                }
+            }
+
+            watch.Stop();
+            Console.WriteLine(watch.Elapsed);
+        }
+    }
+}
