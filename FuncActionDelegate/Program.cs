8c168b73f5279ffd696de953b16437e924b3a204
Zor Max <okocadere@gmail.com>
2021-11-03 22:24:41 +0300

    Iterator Design Pattern

---
+++ FuncActionDelegate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListCollectionSortReverseMethodDemo
+{
+    public class Program
+    {
+
+        public delegate void Omer();
+
+        public static void Main()
+        {
+            List<int> integerList = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+
+            Console.WriteLine(integerList.Where(NewMethod2).FirstOrDefault());
+            Console.WriteLine(integerList.Where(NewMethod()).FirstOrDefault());
+            Console.ReadKey();
+        }
+
+        private static Func<int, bool> NewMethod()
+        {
+            return x => x == 5;
+        }
+
+        private static bool NewMethod2(int X)
+        {
+            return true;
+        }
+
+        public Omer MyMethod()
+        {
+            return MyMethod2;
+        }
+
+        public void MyMethod2()
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+
+}
